b56aa9f933d68c1482448b9b290a3fcd495acf47
Kevin Ho <keho98@gmail.com>
2014-06-03 17:56:50 -0700

    Gets client example derived from oracle example

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -10,6 +10,17 @@ public class ChatServer implements Runnable{
 	public ChatServer(int socketPort) {
 		// TODO Auto-generated constructor stub
 	}
+	public static void main(String args[]) {
+		ChatServer server = null;
+		if(args.length != 1) {
+			System.out.println("Usage: java ChatServer port");
+			System.exit(1);
+		}
+		else {
+	         server = new ChatServer(Integer.parseInt(args[0]));
+	         server.run();
+		}
+	}
 	@Override
 	public void run() {
 		try {
@@ -19,11 +30,11 @@ public class ChatServer implements Runnable{
 			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 			String inputLine, outputLine;
-			outputLine = "";
 			// Initiate conversation with client
 		    out.println("Connected");
 
 		    while ((inputLine = in.readLine()) != null) {
+		    	outputLine = inputLine;
 		        out.println(outputLine);
 		        if (outputLine.equals("Bye."))
 		            break;
@@ -33,16 +44,6 @@ public class ChatServer implements Runnable{
 		}
 		finally {
 			System.out.println("Session complete.");
-		}
-	}
-	public static void main(String args[]) {
-		ChatServer server = null;
-		if(args.length != 1) {
-			System.out.println("Usage: java ChatServer port");
-		}
-		else {
-	         server = new ChatServer(Integer.parseInt(args[0]));
-	         server.run();
 		}
 	}
 }
